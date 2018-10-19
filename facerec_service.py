22baa022409820d5cc1e35407ecdfc5119bcfb61
goverton <graham.overton@gmail.com>
2018-10-19 22:10:45 +0100

    Update facerec_service.py

--- facerec_service.py
+++ facerec_service.py
@@ -144,4 +144,4 @@ if __name__ == "__main__":
 
     # Start app
     print("Starting WebServer...")
-    app.run(host='0.0.0.0', port=8080, debug=False)
+    app.run(host='0.0.0.0', port=9797, debug=False)
